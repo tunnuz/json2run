0f18ea7c97e5f0d7cee6883aab317d753ed4d678
Luca Di Gaspero <l.digaspero@uniud.it>
2021-03-20 10:48:40 +0100

    Finished cherrypicking (hopefully).

--- json2run/__init__.py
+++ json2run/__init__.py
@@ -4,4 +4,6 @@ from batch import *
 from persistent import *
 from experiment import *
 
+VERSION = "0.5.1"
+
 __all__ = ["ParameterList", "ParameterExpression", "Persistent", "Batch", "Race", "Experiment"]
